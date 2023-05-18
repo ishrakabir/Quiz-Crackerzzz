2661ef4f04bae22540a3473fedbe745bd243c38e
Ishrak-Abir <aatharishrak@gmail.com>
2023-05-19 00:52:05 +0600

    finally

--- src/Components/Option/Option.js
+++ src/Components/Option/Option.js
@@ -10,7 +10,7 @@ const Option = ({ option, correctAnswer, eye }) => {
   
   
     return (
-        <div onClick={() =>  setIsActive(!isActive) } className={`${check && 'bg-green-400'} ${isActive ? 'bg-slate-300' : 'bg-white'} border rounded-lg p-5 border-purple-500 text-purple-700 font-semibold text-lg  cursor-pointer`}>
+        <div onClick={() =>  setIsActive(!isActive) } className={`${check && 'bg-green-300'} ${isActive ? 'bg-slate-300' : 'bg-white'} border rounded-lg p-5 border-purple-500 text-purple-700 font-semibold text-lg  cursor-pointer`}>
             <h1>{option}</h1>
         </div>
     );
