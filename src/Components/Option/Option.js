aa89f3b9a347fa3c2714662208e9e1c55db03764
Ishrak-Abir <aatharishrak@gmail.com>
2023-05-19 12:21:51 +0600

    colour changed

--- src/Components/Option/Option.js
+++ src/Components/Option/Option.js
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import './Option.css'
 const Option = ({ option, correctAnswer, eye }) => {
     let check = false;
-  
- 
-    if (eye === true && option === correctAnswer)
+    if (eye && option === correctAnswer)
         check = true;
     const [isActive, setIsActive] = useState(false);
   
   
     return (
-        <div onClick={() =>  setIsActive(!isActive) } className={`${check && 'bg-green-300'} ${isActive ? 'bg-slate-300' : 'bg-white'} border rounded-lg p-5 border-purple-500 text-purple-700 font-semibold text-lg  cursor-pointer`}>
+        <div onClick={() =>  setIsActive(!isActive) } className={`${check && "bg-emerald-300"} ${isActive ? 'bg-slate-300' : 'bg-white'} border rounded-lg p-5 border-purple-500 text-purple-700 font-semibold text-lg  cursor-pointer`}>
             <h1>{option}</h1>
         </div>
     );
